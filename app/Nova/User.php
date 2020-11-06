bd4d09d42bd350735f904af1c42a42b01cfa4a88
Abraham Mendoza <abraham.mendoza@uprevenue.mx>
2020-11-06 10:47:03 -0600

    permisos

--- app/Nova/User.php
+++ app/Nova/User.php
@@ -7,6 +7,8 @@ use Laravel\Nova\Fields\Gravatar;
 use Laravel\Nova\Fields\ID;
 use Laravel\Nova\Fields\Password;
 use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Fields\BelongsToMany;
+use Silvanite\NovaToolPermissions\Role;
 
 class User extends Resource
 {
@@ -65,6 +67,8 @@ class User extends Resource
                 ->onlyOnForms()
                 ->creationRules('required', 'string', 'min:8')
                 ->updateRules('nullable', 'string', 'min:8'),
+            
+            BelongsToMany::make('Roles', 'roles', Role::class),
         ];
     }
 
