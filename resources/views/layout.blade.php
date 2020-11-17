5109f4e3951a1de1244a2079d0913b217a5b938d
JerryWick182 <gerardo.alba@uprevenue.mx>
2020-11-17 16:03:09 -0600

    Se añadió la plantilla

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,89 @@
+<!DOCTYPE HTML>
+<!--
+	Ion by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+	<head>
+		<title>Proveedora Reysi</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-layers.min.js"></script>
+		<script src="js/init.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-xlarge.css" />
+		</noscript>
+	</head>
+	<body id="top">
+
+		<!-- Header -->
+			<header id="header" class="skel-layers-fixed">
+				<h1><a href="/">Reysi</a></h1>
+				<nav id="nav">
+					<ul>
+						<li><a href="/">Inicio</a></li>
+						<!-- <li><a href="left-sidebar.html">Left Sidebar</a></li>
+						<li><a href="right-sidebar.html">Right Sidebar</a></li>
+						<li><a href="no-sidebar.html">No Sidebar</a></li> -->
+						<li><a href="/admin-reysi-2020" class="button special">Iniciar Sesion</a></li>
+					</ul>
+				</nav>
+			</header>
+            
+            @yield ('content')
+
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="container">
+					<div class="row double">
+						<div class="6u">
+							<div class="row collapse-at-2">
+								<div class="6u">
+									<h3>Navega</h3>
+									<ul class="alt">
+										<li><a href="#">Nosotros</a></li>
+										<li><a href="#">Contacto</a></li>
+										<li><a href="#">Devoluciones</a></li>
+                                        <li><a href="#">Privacidad</a></li>
+                                        <li><a href="#">Térmonos y Condiciones</a></li>
+									</ul>
+								</div>
+								<div class="6u">
+									<h3>Categorías</h3>
+									<ul class="alt">
+										<li><a href="#">Nascetur nunc varius</a></li>
+										<li><a href="#">Vis faucibus sed tempor</a></li>
+										<li><a href="#">Massa amet lobortis vel</a></li>
+										<li><a href="#">Nascetur nunc varius</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div class="6u">
+							<h2>Ponte en contacto</h2>
+							<p>Blandit nunc tempor lobortis nunc non. Mi accumsan. Justo aliquet massa adipiscing cubilia eu accumsan id. Arcu accumsan faucibus vis ultricies adipiscing ornare ut. Mi accumsan justo aliquet.</p>
+							<ul class="icons">
+                                <li><a href="#" class="icon fa-phone"><span class="label">Teléfono</span></a></li>
+                                <li><a href="#" class="icon fa-envelope"><span class="label">Correo</span></a></li>
+								<li><a href="https://www.facebook.com/Proveedora-REYSI-237512403124458" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+								<li><a href="https://www.instagram.com/proveedora.reysi/" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+							</ul>
+						</div>
+					</div>
+					<ul class="copyright">
+						<li>&copy; Copyright. Proveedora Reysi</li>
+						<li>Design: <a href="http://uprevenue.mx">Uprevenue.mx</a></li>
+						<!-- <li>Images: <a href="http://unsplash.com">Unsplash</a></li> -->
+					</ul>
+				</div>
+			</footer>
+
+    </body>
+</html>
