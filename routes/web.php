487b9b211e30636fb0fdceca1bd5b1ae5f3b03fd
Abraham Mendoza <abraham.mendoza@uprevenue.mx>
2020-11-17 16:54:53 -0600

    Añadi productos a la homepage

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $products = App\Models\Product::take(3)->latest()->get();
+    return view('welcome', [
+        'products' => $products
+    ]);
 });
 
 Auth::routes();
