9db9393766de8d8e5b877627481ee6eddaf2e277
Abraham Mendoza <abraham.mendoza@uprevenue.mx>
2020-11-18 13:53:50 -0600

    Avances

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,11 +1,11 @@
 @extends ('layout')
 
 @section ('content')
-<div class="row" style="padding-top:75px">
+<div class="row" style="padding:75px;">
     @foreach ($products as $product)
-		<div class="4u">
+		<div class="3u">
 			<section class="special box">
-				<a href="/productos/{{ $product->slug }}" class="image fit"><img src="/storage/{{ $product->image }}" alt="" /></a>
+				<a href="/productos/{{ $product->slug }}" class="image fit"><img src="/storage/{{ $product->image }}" /></a>
 				<h3>{{ $product->name }}</h3>
 				<p>{{ $product->description }}</p>
 				<a href="/productos/{{ $product->slug }}" class="button special">Ver Producto</a>
